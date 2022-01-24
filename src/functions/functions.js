f4e87c60fd08ebb72892d69b5fc3e96a836bde24
AliwerDev <alwerdev@gmail.com>
2022-01-24 14:49:28 +0500

    hello

---
+++ src/functions/functions.js
@@ -0,0 +1,23 @@
+const speak = (text) => {
+	let synth = window.speechSynthesis;
+	let voices = synth.getVoices()
+	let utterance1 = new SpeechSynthesisUtterance(text);
+	utterance1.voice = voices[5];
+	synth.speak(utterance1);
+}
+
+const shuffle = (array) => {
+	let currentIndex = array.length,
+		randomIndex;
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+		[array[currentIndex], array[randomIndex]] = [
+			array[randomIndex],
+			array[currentIndex],
+		];
+	}
+	return array;
+};
+
+export { speak, shuffle };
